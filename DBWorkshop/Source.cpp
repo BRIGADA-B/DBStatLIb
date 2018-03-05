6a7f17d0227cd2a57a03fce6d1155d1c8cfc2c93
VLAD <sinimawath@gmail.com>
2018-03-05 12:54:08 +0300

    Добавил разделение на классы в искодниках изменение

--- DBWorkshop/Source.cpp
+++ DBWorkshop/Source.cpp
@@ -1,8 +1,9 @@
-#include<dbmsLib_v1.h>
+#include<dbmanager.h>
 #include<iostream>
 
-using namespace dbmsLib;
+using namespace dbmanager;
 int main() {
+<<<<<<< HEAD
 	setlocale(LC_ALL, "Russian");
 	string pathToDb = "C:\\Users\\����\\source\\repos\\DBStatLIb\\LibraryTxt\\Students.csv";	
 	DBTableTxt table, tableTest;
@@ -12,7 +13,13 @@ int main() {
 	table.PrintTable(29);
 	
 	PrintTable1(table, 80);
+=======
+>>>>>>> Добавил разделение на классы в искодниках изменение
 
+	setlocale(LC_ALL, "Russian");	
+	DBTableTxt table;
+	string nul;
+	//added line for example
 	char c;
 	cin >> c;
 	return 0;
