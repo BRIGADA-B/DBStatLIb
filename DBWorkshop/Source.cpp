bbe35d36cc1d656a369663e47507c12a69013f61
DronBrigadir <DronBrigadir@users.noreply.github.com>
2018-03-12 20:05:50 +0300

    add my link

---
+++ DBWorkshop/Source.cpp
@@ -0,0 +1,14 @@
+#include <dbmanager.h>
+#include<iostream>
+
+using namespace dbmanager;
+int main() {
+	setlocale(LC_ALL, "Russian");	
+	DBTableTxt table;
+	string pathTo = "D:\\Учеба\\второй семестр\\программирование\\repository\\Project\\LibraryTxt\\Students.csv";
+	table.ReadDBTable(pathTo);
+	table.PrintTable(80);
+	char c;
+	cin >> c;
+	return 0;
+}
